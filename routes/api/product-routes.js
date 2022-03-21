6136371776545bcc290d1cb92d907b2fd7cf124e
Tiffany <leopardmitai@gmail.com>
2022-03-21 00:06:10 -0700

    Added routing for products, working on delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const products = await Product.findAll({
+
+
+  });
+  res.json({success: true, hit: "Get that product!"})
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const product = await Product.findByPk(req.params.id);
+  res.json(product);
 });
 
 // create new product
@@ -25,6 +32,8 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
+    // create new product based on data provided in req.body
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -91,6 +100,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+
+  Product.destroy({
+    where: {
+      product_id: req.params.id,
+    },
+  })
+    .then((deletedProduct) => {
+      res.json(deletedProduct);
+    })
+    .catch((err) => res.json(err));
+
+
 });
 
 module.exports = router;
