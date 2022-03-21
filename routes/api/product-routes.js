1ef76f58407a4f2b3cf1ec7b3bf25a82886c4daf
Tiffany <leopardmitai@gmail.com>
2022-03-21 00:39:12 -0700

    Got delete route to work and added success notifications

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,15 +113,25 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  await Product.destroy({
+  const deletedProduct = await Product.destroy({
     where: {
-      product_id: req.params.id,
+      id: req.params.id,
     },
-  })
-    .then((deletedProduct) => {
-      res.json(deletedProduct);
-    })
-    .catch((err) => res.json(err));
+  });
+
+  if (!deletedProduct) {
+    res
+    .status(404)
+    .json({message: "No such product with this ID."});
+    return;
+  } else {
+    res.json({success: true, message: "Product deleted."})
+  }
+    // .then((deletedProduct) => {
+    //   // res.json(deletedProduct);
+    //   
+    // })
+    // .catch((err) => res.json(err));
 });
 
 module.exports = router;
